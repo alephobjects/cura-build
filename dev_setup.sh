a5289ff64944b9872e501224633e3fc7d822afbe
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2016-11-04 15:51:09 -0400

    Adding script for easy setup of development environment

---
+++ dev_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p build-devenv
+cd build-devenv
+cmake .. && make || exit
+rm -rf ../devenv/bin ../devenv/lib ../devenv/share
+mkdir -p ../devenv
+cp -r inst/bin inst/lib inst/share ../devenv
+cd ../devenv
+git clone https://code.alephobjects.com/diffusion/CT/Cura2.git cura
+git clone https://code.alephobjects.com/diffusion/U/uranium.git
+rm -rf share/uranium share/cura lib/cura lib/uranium lib/python3/dist-packages/cura lib/python3/dist-packages/UM
+cat > cura.sh <<EOF
+#!/bin/bash
+
+LD_LIBRARY_PATH=./lib PYTHONPATH=cura:uranium:./lib/python3/dist-packages QT_PLUGIN_PATH=./lib/plugins QML2_IMPORT_PATH=./lib/qml QT_QPA_FONTDIR=./lib/fonts python3.4 ./bin/cura2 "$@"
+EOF
+chmod +x ./cura.sh
