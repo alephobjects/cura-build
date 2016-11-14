ec002032f34c9967c7f17416727177b40d46bd21
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2016-11-14 15:57:21 -0500

    Escape $@ in the cura.sh script

--- dev_setup.sh
+++ dev_setup.sh
@@ -13,6 +13,6 @@ rm -rf share/uranium share/cura lib/cura lib/uranium lib/python3/dist-packages/c
 cat > cura.sh <<EOF
 #!/bin/bash
 
-LD_LIBRARY_PATH=./lib PYTHONPATH=cura:uranium:./lib/python3/dist-packages QT_PLUGIN_PATH=./lib/plugins QML2_IMPORT_PATH=./lib/qml QT_QPA_FONTDIR=./lib/fonts python3.4 ./bin/cura2 "$@"
+LD_LIBRARY_PATH=./lib PYTHONPATH=cura:uranium:./lib/python3/dist-packages QT_PLUGIN_PATH=./lib/plugins QML2_IMPORT_PATH=./lib/qml QT_QPA_FONTDIR=./lib/fonts python3.4 ./bin/cura2 "\$@"
 EOF
 chmod +x ./cura.sh
