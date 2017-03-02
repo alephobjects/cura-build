8e817d86b0cc8544a4e2843ccb273638193be02c
Alexei Dorofeev <alexei@alephobjects.com>
2017-03-02 13:57:36 -0700

    Changed the versioning format to add git hashes for all internal repos.

--- scripts/cura_version.py
+++ scripts/cura_version.py
@@ -7,12 +7,20 @@ def get_commit_hash(project, cmake_binary_dir):
     result = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=project_path)
     return result.decode("utf-8").replace('\n', '')
 
+def get_curabuild_commit_hash(cmake_binary_dir):
+    project_path = cmake_binary_dir + "/.."
+    result = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=project_path)
+    return result.decode("utf-8").replace('\n', '')
+
 def generate(cura_version, cmake_binary_dir, target_dir):
     data = {
-        "cura": cura_version,
+        "cura_version": cura_version,
+	"cura": get_commit_hash("Cura", cmake_binary_dir),
         "uranium": get_commit_hash("Uranium", cmake_binary_dir),
         "engine": get_commit_hash("CuraEngine", cmake_binary_dir),
-        "libarcus": get_commit_hash("Arcus", cmake_binary_dir)
+        "libarcus": get_commit_hash("Arcus", cmake_binary_dir),
+        "binarydata":get_commit_hash("cura-binary-data", cmake_binary_dir),
+	"build": get_curabuild_commit_hash(cmake_binary_dir)
     }
 
     with open(target_dir + "/version.json", 'w') as output_file:
