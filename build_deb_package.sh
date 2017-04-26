65dc2360f2cf3354206956fa98f93ac1e269f96e
Alexei Dorofeev <alexei@alephobjects.com>
2017-04-25 23:38:02 -0600

    Major Python build rework via python build with cmake.

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir -p build
+
 cd build
 cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_TAG_OR_BRANCH=devel -DURANIUM_TAG_OR_BRANCH=devel ../
 echo "You should be able to monitor progress via \"tail -f `pwd`/cura2_build.log\" in another terminal"
-PYTHONPATH=`pwd`/inst/lib/python3.5/site-packages PATH=`pwd`/inst/bin:${PATH} LD_LIBRARY_PATH=`pwd`/inst/lib make package &> cura2_build.log
+CC=gcc CXX=g++ F90=gfortran PYTHONPATH=`pwd`/inst/lib/python3.5/site-packages PATH=`pwd`/inst/bin:${PATH} LD_LIBRARY_PATH=`pwd`/inst/lib make package &> cura2_build.log
 cd ../
 # tail -f cura2_build.log
