9023defe16775044b5c732869cc9f5bce66e0910
Vladimir Tomanovski <vladoohr@gmail.com>
2017-06-09 17:12:18 +0200

    Build firmware

--- docker/scripts/build_ultimaker.sh
+++ docker/scripts/build_ultimaker.sh
@@ -14,12 +14,25 @@ function build_fdm_materials () {
 function build_cbd () {
   revision=$CBD_GIT_REVISION
 
-  cd /Cura2build/ultimaker
-  git clone $CBD_GIT_REPO cura-binary-data
-  cd cura-binary-data/
-  git checkout -f $revision
+  CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=/Cura2build/ultimaker/build/ultimaker-1.0.0/usr"
+
+  if [ ! -z "$CBD_BUILD_MARLIN" ]; then
+    CMAKE_ARGS="$CMAKE_ARGS -DBUILD_MARLIN_FIRMWARES=$CBD_BUILD_MARLIN"
+  fi
+
+  cd /Cura2build
+
+  if [ ! -d "build/cura-binary-data" ]; then
+    cd ultimaker
+    git clone $CBD_GIT_REPO cura-binary-data
+    cd cura-binary-data/
+    git checkout -f $revision
+  else
+    cd build/cura-binary-data
+  fi
+
   mkdir build && cd build
-  cmake -DCMAKE_INSTALL_PREFIX=/Cura2build/ultimaker/build/ultimaker-1.0.0/usr ..
+  cmake $CMAKE_ARGS ..
   make
   make install
 }
