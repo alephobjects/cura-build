954f66f590431bd13bc0520a78c71cdb96fa56b6
Alexei Dorofeev <alexei@alephobjects.com>
2017-07-25 09:13:00 -0600

    Change default branches to master for Cura2 and Uranium repos.

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd build
-cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_TAG_OR_BRANCH=devel -DURANIUM_TAG_OR_BRANCH=devel ../
+cmake -DMINIMUM_PYTHON_VERSION=3.4.0 ../
+## For trying out model Subdivider
+# cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_TAG_OR_BRANCH=model_subdivider -DURANIUM_TAG_OR_BRANCH=model_subdivider ../
+## For trying out SciPy Lite
+# cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_TAG_OR_BRANCH=master -DURANIUM_TAG_OR_BRANCH=master -DBUILD_SCIPY=OFF -DBUILD_SCIPY_LITE=ON ../
 echo "You should be able to monitor progress via \"tail -f `pwd`/cura2_build.log\" in another terminal"
 CC=gcc CXX=g++ F90=gfortran PYTHONPATH=`pwd`/inst/lib/python3.5/site-packages PATH=`pwd`/inst/bin:${PATH} LD_LIBRARY_PATH=`pwd`/inst/lib make package &> cura2_build.log
 cd ../
