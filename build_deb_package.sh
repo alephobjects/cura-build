516dc6ff58f2b3cb6fc06340bc4c3381300d0a43
Alexei Dorofeev <alexei@alephobjects.com>
2017-09-06 11:25:43 -0600

    Added example for 2.7 branch build.

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -6,6 +6,8 @@ cmake -DMINIMUM_PYTHON_VERSION=3.4.0 ../
 # cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_TAG_OR_BRANCH=model_subdivider -DURANIUM_TAG_OR_BRANCH=model_subdivider ../
 ## For trying out SciPy Lite
 # cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_TAG_OR_BRANCH=master -DURANIUM_TAG_OR_BRANCH=master -DBUILD_SCIPY=OFF -DBUILD_SCIPY_LITE=ON ../
+## For trying 2.7 branch
+# cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_ENGINE_TAG_OR_BRANCH=upstream2.7 -DCURA_TAG_OR_BRANCH=upstream2.7 -DURANIUM_TAG_OR_BRANCH=upstream2.7 ../
 echo "You should be able to monitor progress via \"tail -f `pwd`/cura2_build.log\" in another terminal"
 CC=gcc CXX=g++ F90=gfortran PYTHONPATH=`pwd`/inst/lib/python3.5/site-packages PATH=`pwd`/inst/bin:${PATH} LD_LIBRARY_PATH=`pwd`/inst/lib make package &> cura2_build.log
 cd ../
