cc4ec31bee5b6420391309535f5e651263ed7765
Alexei Dorofeev <alexei@alephobjects.com>
2017-12-05 08:21:39 -0700

    Changed branches to upstream3.1 for DEB package build script.

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd build
-cmake ../
+# cmake ../
 ## For trying out model Subdivider
 # cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_TAG_OR_BRANCH=model_subdivider -DURANIUM_TAG_OR_BRANCH=model_subdivider ../
 ## For trying out SciPy Lite
 # cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_TAG_OR_BRANCH=master -DURANIUM_TAG_OR_BRANCH=master -DBUILD_SCIPY=OFF -DBUILD_SCIPY_LITE=ON ../
-## For trying 2.7 branch
-# cmake -DMINIMUM_PYTHON_VERSION=3.4.0 -DCURA_ENGINE_TAG_OR_BRANCH=upstream2.7 -DCURA_TAG_OR_BRANCH=upstream2.7 -DURANIUM_TAG_OR_BRANCH=upstream2.7 ../
+## For trying 3.1 branch
+cmake -DCURA_ENGINE_TAG_OR_BRANCH=upstream3.1 -DCURA_TAG_OR_BRANCH=upstream3.1 -DURANIUM_TAG_OR_BRANCH=upstream3.1 ../
 echo "You should be able to monitor progress via \"tail -f `pwd`/cura-lulzbot_build.log\" in another terminal"
 CC=gcc CXX=g++ F90=gfortran PYTHONPATH=`pwd`/inst/lib/python3.5/site-packages PATH=`pwd`/inst/bin:${PATH} LD_LIBRARY_PATH=`pwd`/inst/lib make package &> cura-lulzbot_build.log
 cd ../
